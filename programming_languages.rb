c52c2ec8966b5b8b90a242e55fe945d89f6c67ba
Matt Zuzolo <mattzuzolo@outlook.com>
2018-05-01 16:19:01 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -6,29 +6,30 @@ def reformat_languages(languages)
     new_hash = {}
     type_array = []
   
-  
     languages.each do |type, data|
-      
-    binding.pry
-      type_array << type  
-      
-  
-      data.each do |programming_language, type_with_value|
+        data.each do |programming_language, type_with_value|
           
-          new_hash[programming_language] = type_with_value
-          new_hash[:style] = type_array
- 
-      end #end data each
+          
+
+            type_with_value.each do |key, value|
+                
+                
+                type_array << type 
+                #new_hash[programming_language] = {key => value, :style => type_array }
+                new_hash[programming_language] = {key => value}
+          
+                
+            end
+            
+            new_hash[programming_language] = {:style => type_array }
         
-       
-          
-          
+    binding.pry
+
+        end #end data each
+        
      
     end #end languages.each
-
-binding.pry
-     new_hash
-     
+    new_hash
 end #end method
 
 
@@ -37,7 +38,20 @@ end #end method
 
 
 
-
+     
+          # new_hash[programming_language] = type_with_value
+          #   new_hash[:style] = type_array
+          
+          
+          # new_hash[big_key] = {
+          
+          # type_with_value.each do |key, value|
+            
+          #   new_hash[key] = value
+            
+          #   binding.pry
+            
+          # end
 
 
 
