132b90ee60a78a0cb110678fca4e5d33d802bd63
Matt Zuzolo <mattzuzolo@outlook.com>
2018-05-01 15:43:06 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,36 +1,35 @@
 require 'pry'
-$first_level = nil
+
 
 def reformat_languages(languages)
  
+    new_hash = {}
+    type_array = []
+  
+  
+    languages.each do |type, data|
+      type_array << type  
+      
+        data.each do |programming_language, type_with_value|
+          
+          new_hash[programming_language] = type_with_value
+          
+          
+          new_hash[:style] = type_array
+        end #end data each
+      end #end languages.each
 
-  new_hash = {}
-  type_array = []
-  
-  
-languages.each do |type, data|
-    #binding.pry
-    #key is oo
-    #value is data
-    
-    data.each do |programming_language, type_with_value|
-      new_hash[programming_language] = type_with_value
-      $first_level = programming_language
-    end #end data each
-  end #end languages.each
-  
-languages.each do |type, data|
-  
-    new_hash[$first_level] = type
-    binding.pry
-  
-end #end languages each (second time)  
-  
  
- 
- 
-  #return new_hash at end of method
-end
+    # languages.each do |type, data|
+      
+    #     type_array << type  
+    #     new_hash[:style] = type_array
+        
+    # end #end languages each (second time)  
+      
+     new_hash
+     binding.pry
+end #end method
 
 
 
