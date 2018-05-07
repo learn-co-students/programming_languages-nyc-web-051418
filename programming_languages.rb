3de5d4ca418266027ff756a7bd38c14a61e39656
Kayla Fitzgerald <fitzgerald.km11@gmail.com>
2018-05-06 23:55:27 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,12 @@
-def reformat_languages(languages)
-  # your code here
+require 'pry'
+def reformat_languages(language_data)
+  new_hash = {}
+  language_data.each do |style, languages|
+    languages.each do |lang_name, lang_data|
+      new_hash[lang_name] ||= lang_data
+      new_hash[lang_name][:style] ||= []
+      new_hash[lang_name][:style].push(style)
+    end
+  end
+  new_hash
 end
