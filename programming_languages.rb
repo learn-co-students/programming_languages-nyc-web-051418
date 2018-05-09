ade2f88ea11d6917fd6760e2d4dc865c6b7f7b8c
Jordan Eckowitz <jordan.eckowitz@gmail.com>
2018-05-09 16:19:12 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+new_hash = {}
+languages.each do |style,langs|
+  langs.each do |lang,type|
+    type[:style] = [style]
+    # new_hash[lang] = type
+    if new_hash[lang]
+      new_hash[lang][:style] << style
+    else
+      new_hash[lang] = type
+    end
+  end
+end
+return new_hash
+  
 end
